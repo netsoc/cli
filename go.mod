8c9551e4789286893ecd85702928b2013eb18f45
Jack O'Sullivan <jackos1998@gmail.com>
2020-09-26 23:20:14 +0100

    Update `login` command to allow logging out other users

--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/dgrijalva/jwt-go/v4 v4.0.0-preview1
 	github.com/githubnemo/CompileDaemon v1.2.1
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/netsoc/iam/client v1.0.5
+	github.com/netsoc/iam/client v1.0.6
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.7.1
--- pkg/cmd/account/logout.go
+++ pkg/cmd/account/logout.go
@@ -1,24 +1,31 @@
 package account
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"log"
 
 	"github.com/netsoc/cli/pkg/config"
 	"github.com/netsoc/cli/pkg/util"
+	iam "github.com/netsoc/iam/client"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 type logoutOptions struct {
-	Config func() (*config.Config, error)
+	Config    func() (*config.Config, error)
+	IAMClient func() (*iam.APIClient, error)
+
+	All  bool
+	User string
 }
 
 // NewCmdLogout creates a new account logout command
 func NewCmdLogout(f *util.CmdFactory) *cobra.Command {
 	opts := logoutOptions{
-		Config: f.Config,
+		Config:    f.Config,
+		IAMClient: f.IAMClient,
 	}
 	cmd := &cobra.Command{
 		Use:   "logout",
@@ -27,6 +34,9 @@ func NewCmdLogout(f *util.CmdFactory) *cobra.Command {
 			return logoutRun(opts)
 		},
 	}
+
+	cmd.Flags().BoolVar(&opts.All, "all", false, "log out of all devices")
+	cmd.Flags().StringVarP(&opts.User, "user", "u", "self", "(admin only) user to log out, only applicable with --all")
 
 	return cmd
 }
@@ -41,12 +51,36 @@ func logoutRun(opts logoutOptions) error {
 		return errors.New("not logged in")
 	}
 
+	if opts.All {
+		client, err := opts.IAMClient()
+		if err != nil {
+			return err
+		}
+		ctx := context.WithValue(context.Background(), iam.ContextAccessToken, c.Token)
+
+		u, _, err := client.UsersApi.GetUser(ctx, "self")
+		if err != nil {
+			return fmt.Errorf("failed to get info about user: %w", util.APIError(err))
+		}
+
+		_, err = client.UsersApi.Logout(ctx, opts.User)
+		if err != nil {
+			return util.APIError(err)
+		}
+
+		if opts.User != "self" && opts.User != u.Username {
+			log.Println("Logged out successfully")
+			return nil
+		}
+	} else if opts.User != "self" {
+		return errors.New("user provided but `--all` not passed")
+	}
+
 	viper.Set("token", "")
 	if err := viper.WriteConfig(); err != nil {
 		return fmt.Errorf("failed to write config: %w", err)
 	}
 
 	log.Println("Logged out successfully")
-
 	return nil
 }
